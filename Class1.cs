6d11e798133fdee21be116550e3239e6c19824cd
Denis Riabchuk <Naimas@DESKTOP-68Q4MHG>
2023-11-18 13:55:47 +0200

    done2

---
+++ Class1.cs
@@ -0,0 +1,56 @@
+﻿public class FoodItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; } // Овоч або фрукт
+    public string Color { get; set; }
+    public double Calories { get; set; }
+}
+
+public class FoodDbContext : DbContext
+{
+    public DbSet<FoodItem> FoodItems { get; set; }
+}
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть 1 для підключення або 2 для від'єднання від бази даних:");
+        string userInput = Console.ReadLine();
+
+        if (userInput == "1")
+        {
+            ConnectToDatabase();
+        }
+        else if (userInput == "2")
+        {
+            DisconnectFromDatabase();
+        }
+        else
+        {
+            Console.WriteLine("Невірний ввід.");
+        }
+    }
+
+    static void ConnectToDatabase()
+    {
+        using (var context = new FoodDbContext())
+        {
+            try
+            {
+                context.Database.EnsureCreated();
+                Console.WriteLine("Підключено до бази даних.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка підключення до бази даних: {ex.Message}");
+            }
+        }
+    }
+
+    static void DisconnectFromDatabase()
+    {
+        Console.WriteLine("Від'єднано від бази даних.");
+    }
+}
--- DbContext.cs
+++ DbContext.cs
@@ -1,3 +1,9 @@
 ﻿public class DbContext
 {
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Встановіть рядок підключення до вашої бази даних
+        optionsBuilder.UseSqlServer("YourConnectionString");
+    }
 }
